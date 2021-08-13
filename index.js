94e8c1efeb64a2f631f5e09ae59a6af246cbf8fa
Aro <88704628+thearo665@users.noreply.github.com>
2021-08-14 00:21:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-FiiX.login('');
+FiiX.login('ODc1Nzk5NjIxNTQzMjg4ODYy.YRax9Q.RIv52L8Uy1VAOIdVFu290gORX38');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
